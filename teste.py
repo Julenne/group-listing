18545c015a4d80115320ec0d2b5b43daf4b0b9a0
Arlene Pelenda <arlenej011@gmail.com>
2021-05-19 15:59:44 -0300

    python

--- teste.py
+++ teste.py
@@ -1,19 +1,18 @@
 import logging
 import configparser
-import telepot
-from telegram.ext import Updater, CommandHandler, MessageHandler
-
-text_newlist = ""
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+ 
+ext_newlist = ""
 text_sent = ""
 array = []
 config = configparser.ConfigParser()
 config.read('config.ini')
 updater = Updater(token=config['DEFAULT']['token'], use_context=True)
 dispatcher = updater.dispatcher
-
+ 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.INFO)
-
+ 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, 
         text="Olá, seja bem vindo(a) ao Group Listing Bot."
@@ -21,10 +20,10 @@ def start(update, context):
         + "\n Para começar, adicione este bot à um grupo. Para utilizar:"
         + "\n /newlist - Para criar uma nova lista "
         + "\n /add - Para adicionar um elemento à lista")
-
+ 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
-
+ 
 def newlist(update, context):
         global text_newlist
         global text_sent
@@ -34,18 +33,15 @@ def newlist(update, context):
         
 newlist_handler = CommandHandler('newlist', newlist)
 dispatcher.add_handler(newlist_handler)
-
-
+ 
+ 
 def listing(update, context):
         array.append(update.message.text)    
         context.bot.edit_message_text(chat_id=update.effective_chat.id, 
                 message_id=text_sent.message_id,
                 text=text_sent.text + "\n" + '\n'.join(array))
         context.bot.delete_message(chat_id=update.effective_chat.id, message_id = update.message.message_id)
-    #context.bot.send_message(chat_id=update.effective_chat.id, text=text_newlist + "\n" + str(array))
-listing_handler = CommandHandler('add', listing)
-dispatcher.add_handler(listing_handler)
-
-
-
+ 
+dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, listing))
+ 
 updater.start_polling()
